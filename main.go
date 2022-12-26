bf008d56f2351b7d883d763ce75152e5539d2d4e
wkd-woo <wkdwoos@gmail.com>
2022-12-26 16:04:43 +0900

    ch2: var and conditions

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	price := 100
-	fmt.Println("Price is ", price, "dollors")
-
-	taxRate := 0.08
-	tax := price * int(taxRate)
-
-	fmt.Println("Tax is ", tax, "dollors.")
-	total := price + tax
-	fmt.Println("Total cost is ", total)
-
-	availableFunds := 120
-	fmt.Println(availableFunds, "dollors available")
-	fmt.Println("Within budget?", total <= availableFunds)
+	broken := "G# r#cks!"
+	replacer := strings.NewReplacer("#", "o")
+	fixed := replacer.Replace(broken)
+	fmt.Println(fixed)
 }
